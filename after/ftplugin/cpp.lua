e9a8d5c2d3551298ab2c917fc96043b8e83a511c
Gabriel Peterson <25187859+gabrieldpeterson@users.noreply.github.com>
2025-03-10 14:16:02 -0700

    Add C++ configuration

---
+++ after/ftplugin/cpp.lua
@@ -0,0 +1,14 @@
+-- Enable automatic C-style indentation
+vim.bo.cindent = true
+
+-- Set the number of spaces for each indentation level
+vim.bo.shiftwidth = 4
+
+-- Set the number of spaces that a <Tab> in the file counts for
+vim.bo.tabstop = 4
+
+-- Use spaces instead of tabs
+-- vim.bo.expandtab = true
+
+-- Customize C indentation options
+vim.bo.cinoptions = ':0,l1,t0,(0'
